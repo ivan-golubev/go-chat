b967150cef93c5785888eaae495695376343df5f
Ivan Golubev <igolubev@ea.om>
2016-01-07 21:56:26 +0300

    temp code 2
    adding terminal clear function
    FSM implementation is required ASAP

--- client-fsm/fsm.go
+++ client-fsm/fsm.go
@@ -3,9 +3,11 @@ package main
 import (
     "fmt"
     "os"
+    "os/exec"
     "bufio"
     "strings"
     "syscall"
+    "runtime"
     "golang.org/x/crypto/ssh/terminal"
 )
 
@@ -18,6 +20,7 @@ func check_error(err error) {
 
 func main() {
 	username, _ := credentials()
+	clear_cmd()
 	fmt.Printf("\nWelcome %s!\n", username)
 }
 
@@ -35,4 +38,36 @@ func credentials() (string, string) {
 	password := string(bytePassword)
 
 	return strings.TrimSpace(username), strings.TrimSpace(password)
+}
+
+// the below code was borrowed from here: 
+// http://stackoverflow.com/questions/22891644/how-can-i-clear-the-terminal-screen-in-go 
+var clear map[string]func() //create a map for storing clear funcs
+
+func init() {
+    clear = make(map[string]func()) //Initialize it
+    clear["linux"] = func() { 
+        cmd := exec.Command("clear") //Linux example, its tested
+        cmd.Stdout = os.Stdout
+        cmd.Run()
+    }
+    clear["darwin"] = func() { 
+        cmd := exec.Command("clear") //os x example, tested by ivan
+        cmd.Stdout = os.Stdout
+        cmd.Run()
+    }
+    clear["windows"] = func() {
+        cmd := exec.Command("cls") //Windows example it is untested, but I think its working 
+        cmd.Stdout = os.Stdout
+        cmd.Run()
+    }
+}
+
+func clear_cmd() {
+    value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
+    if ok { //if we defined a clear func for that platform:
+        value()  //we execute it
+    } else { //unsupported platform
+        panic("Your platform is unsupported! I can't clear terminal screen :(")
+    }
 }
