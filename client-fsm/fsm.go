fe57bd5e53f09a15383ba6ae292c461a120bdaa6
Ivan Golubev <igolubev@ea.om>
2016-01-08 21:47:50 +0300

    client FSM
    Implementing the client State Machine

--- client-fsm/fsm.go
+++ client-fsm/fsm.go
@@ -11,6 +11,48 @@ import (
     "golang.org/x/crypto/ssh/terminal"
 )
 
+type State interface {
+	Init()
+	LoginSuccess(string, int)
+	LoginFailed()
+	// FriendInvited()
+	Logout()
+}
+
+type InitState struct {}
+func (this *InitState) Init(){
+	username, password := credentials()
+	// TODO: perform a TCP call to our central server to fetch the user id
+	user_id := 42
+	// just a trivial check: actually this should be done on the server
+	if (username == "" || password == "") { // if status is success
+		fsm.LoginFailed()
+	} else {
+		fsm.LoginSuccess(username, user_id)
+	}
+}
+func (this *InitState) LoginSuccess(usr_name string, usr_id int){
+	fsm = &AuthenticatedState{user_name: usr_name, user_id: usr_id}
+	fsm.Init()
+}
+func (this *InitState) LoginFailed(){
+	fmt.Println("\nError: cannot login with the provided credentials.")
+}
+func (this *InitState) Logout(){}
+
+
+type AuthenticatedState struct {
+	user_name string
+	user_id int
+}
+func (this *AuthenticatedState) Init(){
+	clear_cmd()
+	fmt.Printf("\nWelcome %s!\n", this.user_name)
+}
+func (this *AuthenticatedState) LoginSuccess(_ string, _ int){}
+func (this *AuthenticatedState) LoginFailed(){}
+func (this *AuthenticatedState) Logout(){}
+
 func check_error(err error) {
     if err != nil {
         fmt.Println("Error: " , err)
@@ -18,10 +60,10 @@ func check_error(err error) {
     }
 }
 
+var fsm State = &InitState{}
+
 func main() {
-	username, _ := credentials()
-	clear_cmd()
-	fmt.Printf("\nWelcome %s!\n", username)
+	fsm.Init()
 }
 
 func credentials() (string, string) {
